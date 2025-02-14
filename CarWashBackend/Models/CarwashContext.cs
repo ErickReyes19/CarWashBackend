bbe68313abc9290b838183fd6791597bdbde9c77
ereyes0212 <erick.reyes@tiempo.hn>
2025-02-14 11:59:12 -0600

    Update CarwashContext.cs

--- CarWashBackend/Models/CarwashContext.cs
+++ CarWashBackend/Models/CarwashContext.cs
@@ -42,9 +42,18 @@ public partial class CarwashContext : DbContext
 
     public virtual DbSet<registro_servicio_vehiculo> registro_servicio_vehiculos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=Carwash_DB;uid=root;pwd=P@ssWord.123", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    var mysqlHost = Environment.GetEnvironmentVariable("MYSQL_HOST") ?? "localhost";
+    var mysqlDatabase = Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? "Carwash_DB";
+    var mysqlUser = Environment.GetEnvironmentVariable("MYSQL_USER") ?? "root";
+    var mysqlPassword = Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "P@ssWord.123";
+    
+    var connectionString = $"server={mysqlHost};database={mysqlDatabase};uid={mysqlUser};pwd={mysqlPassword}";
+
+    optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+}
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
